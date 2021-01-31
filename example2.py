e7c2bf9bf4cfb76bd43b1cc001ef4bac8e8d8a99
Administrator <2635681517@qq.com>
2021-02-01 07:27:56 +0800

    learn

--- example2.py
+++ example2.py
@@ -17,13 +17,15 @@ config = {
 con = pymysql.connect(**config)
 username = "1 or 1=1"
 password = "1 or 1=1"
+# sql可以防止sql注入,不向sql语句传入具体的数据，先编译在传值用函数参数形式就可以解决
 sql = "select count(*) from t_user where username = %s " \
       " AND AES_DECRYPT(UNHEX(password),'helloworld')=%s"
+
 sql1 = "select count(*) from t_user where username =" + username + \
        " AND AES_DECRYPT(UNHEX(password),'helloworld')=" + password
 cur = con.cursor()
-# cur.execute(sql % (username, password))
-cur.execute(sql1)
+cur.execute(sql, (username, password))
+# cur.execute(sql1)
 print(cur.fetchone()[0])
 con.close()
 cur.close()
